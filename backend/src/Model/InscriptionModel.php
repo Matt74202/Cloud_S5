f135163f08f5c8f9e64ef48606d365b01af82c9a
Matthew <matthewbogosy@gmail.com>
2025-01-03 12:33:25 +0300

    insertInscription

---
+++ backend/src/Model/InscriptionModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model;
+
+use App\Helper\DatabaseConnection;
+
+class InscriptionModel
+{
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connection = DatabaseConnection::getConnection();
+    }
+
+    public function InsertInscription($nom,$email,$mdp): array{
+        $valide = false;
+        $query = "insert into Inscription(nom,email,mdp,valide) values(:nom ,:email,:mdp,:valide ) ";
+        $stmt = $this->connection->executeQuery($query, ['nom' =>$nom, 'email'=>$email , 'mdp'=> $mdp , 'valide'=>$valide]);
+        return $stmt->fetchAssociative();
+    }
+
+}

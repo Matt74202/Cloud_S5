112bd1b084f0be04e764e59a9504bcd9b39c41d6
Matthew <matthewbogosy@gmail.com>
2025-01-04 14:55:33 +0300

    last

--- backend/src/Model/InscriptionModel.php
+++ backend/src/Model/InscriptionModel.php
@@ -8,23 +8,25 @@ class InscriptionModel
 {
     private $connection;
 
+
     public function __construct()
     {
         $this->connection = DatabaseConnection::getConnection();
+        
     }
 
-    public function InsertInscription($nom,$email,$mdp): array{
+    public function InsertInscription($nom,$email,$mdp){
         $valide = false;
         $query = "insert into Inscription(nom,email,mdp,valide) values(:nom ,:email,:mdp,:valide ) ";
         $stmt = $this->connection->executeQuery($query, ['nom' =>$nom, 'email'=>$email , 'mdp'=> $mdp , 'valide'=>$valide]);
         return $stmt->fetchAssociative();
     }
 
-    public function getById($idInscription): array{
+    public function getById($idInscription){
         $query = "SELECT * FROM Inscription WHERE idInscription = :idInscription ";
         $stmt = $this->connection->executeQuery($query, ['idInscription' => $idInscription]);
         return $stmt->fetchAssociative(); 
     }
-    
+
 
 }

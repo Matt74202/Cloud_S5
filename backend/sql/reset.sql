499cfe25463ed60f854a434da0e168de3d0aeb69
NalyAndriam <153374026+NalyAndriam@users.noreply.github.com>
2025-01-11 13:01:45 +0300

    reset_tentative() sql function

--- backend/sql/reset.sql
+++ backend/sql/reset.sql
@@ -19,3 +19,14 @@ BEGIN
     ALTER SEQUENCE pin_id_seq RESTART WITH 1;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION reset_tentative() 
+RETURNS void AS $$
+BEGIN
+    -- Supprimer toutes les lignes dans la table Inscription
+    DELETE FROM Tentative;
+
+    -- Réinitialiser l'incrémentation de l'ID (la séquence)
+    ALTER SEQUENCE tentative_id_seq RESTART WITH 1;
+END;
+$$ LANGUAGE plpgsql;

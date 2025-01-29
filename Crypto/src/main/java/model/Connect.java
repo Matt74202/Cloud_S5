a17aa176e89b26271c4d7632cbfb7a9356f51de2
Matthew <matthewbogosy@gmail.com>
2025-01-29 09:06:34 +0300

    skelety

---
+++ Crypto/src/main/java/model/Connect.java
@@ -0,0 +1,14 @@
+package com.cloud.Crypto.model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    private static final String URL = "jdbc:postgresql://localhost:5432/crypto"; 
+    private static final String USER = "postgres"; 
+    private static final String PASSWORD = "itu16"; 
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}

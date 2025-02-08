031fe632ab6fd6a2eedcfc95425a38a05482f3ca
NalyAndriam <153374026+NalyAndriam@users.noreply.github.com>
2025-02-08 18:52:21 +0300

    update v_total_vente et v_total_achat (debugging)

--- Crypto/sql/003-view.sql
+++ Crypto/sql/003-view.sql
@@ -57,9 +57,9 @@ JOIN
 JOIN 
     v_crypto_last v ON m.id_crypto = v.id
 JOIN 
-    TypeTransaction t ON m.id = t.id
+    TypeTransaction t ON m.id_type = t.id
 WHERE 
-    t.id = 2  
+    m.id_type = 2  
 GROUP BY 
     u.id, u.nom;
 
@@ -76,9 +76,9 @@ JOIN
 JOIN 
     v_crypto_last v ON m.id_crypto = v.id
 JOIN 
-    TypeTransaction t ON m.id = t.id
+    TypeTransaction t ON m.id_type = t.id
 WHERE 
-    t.id = 1
+    m.id_type = 1
 GROUP BY 
     u.id, u.nom;
 
